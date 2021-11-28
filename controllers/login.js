98a759f809903a5468f56f27d699ad06b3c70150
syednaumanuddin <55260547+syednaumanuddin@users.noreply.github.com>
2021-11-28 17:39:50 +0530

    service and maintainance

--- controllers/login.js
+++ controllers/login.js
@@ -19,19 +19,37 @@ return res.status(401).send("Incorrect Password")
 else
 {
    
+const nodemailer = require('nodemailer');
+    let mailTransporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'sender.liftronic@gmail.com',
+            pass: 'xnteglcqiorzcbbl'
+        }
+    });
+      const ldate=new Date();
+    let mailDetails = {
+        from: 'sender.liftronic@gmail.com',
+        to: 'abdulfaiz11553@gmail.com ',
+        subject: "Login Alert",
+        text: req.body.message+"there was a Login in admin account  At  : "+ldate
+    };
+      
+    mailTransporter.sendMail(mailDetails, function(err, data) {
+     
+     
+    });
     req.session.success="Intern@lC@ll";
+    
   
-res.redirect("/additem")
+    res.redirect("/additem")
 
 }
 
 
-
-
-
-
-
+    
 }
+
 
 const logout=(req,res,next)=>{
 
